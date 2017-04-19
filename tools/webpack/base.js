415c4cee4a74a3349e76ed19f2db254724161748
kaave <abe@framelunch.jp>
2017-04-19 11:10:26 +0900

    Change build dir

--- tools/webpack/base.js
+++ tools/webpack/base.js
@@ -12,7 +12,7 @@ module.exports = {
       ]
     },
     output: {
-      path: path.join(__dirname, '../../build'),
+      path: path.join(__dirname, '..', '..', 'docs'),
       filename: '[name].js',
       publicPath: '/'
     },
